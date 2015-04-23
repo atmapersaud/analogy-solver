1fd5be0c81c7f11556bf38a6a7ef1985adffbd76
Hari Krishn Gupta <hkrishn22@gmail.com>
2015-04-23 14:23:49 -0700

    Started module to answer open ended entity questions
    Implemented checkExists function

--- semantic-module/src/main/java/SemanticAnalysisModule.java
+++ semantic-module/src/main/java/SemanticAnalysisModule.java
@@ -21,34 +21,79 @@ public class SemanticAnalysisModule {
 	public static void main(String[] args) throws RepositoryException, FileNotFoundException {
 		// TODO Auto-generated method stub
 		System.out.println("test");
-		String word1 = "Los_Angeles", word2 = "university_of_southern_california";
+//		String word1 = "Los_Angeles", word2 = "university_of_southern_california";
+//		List<String> linksWord1 = getResourceLinksForWord(word1);
+//		List<String> linksWord2 = getResourceLinksForWord(word2);
+//		Set<String> word1ToWord2 = new HashSet<String>();
+//		Set<String> word2ToWord1 = new HashSet<String>();
+//		int count = 1;
+//		
+//		linksWord1.add("http://dbpedia.org/resource/"+word1);
+//		linksWord2.add("http://dbpedia.org/resource/"+word2);
+//		
+//		for(String s : linksWord1){
+//			for(String s2 : linksWord2){
+//				word1ToWord2.addAll(directRelations(s,s2));
+//				word2ToWord1.addAll(directRelations(s2,s));
+//				System.out.println(count++);
+//			}
+//		}
+//		PrintWriter out = new PrintWriter("temp.txt");
+//		out.println(word1+" - "+word2);
+//		for(String s : word1ToWord2)
+//			out.println(s);
+//		out.println("--------------------------");
+//		out.println(word2+" - "+word1);
+//		for(String s : word2ToWord1)
+//			out.println(s);
+//		out.close();
+		
+		String word1 = "Bill_Gates";
 		List<String> linksWord1 = getResourceLinksForWord(word1);
-		List<String> linksWord2 = getResourceLinksForWord(word2);
-		Set<String> word1ToWord2 = new HashSet<String>();
-		Set<String> word2ToWord1 = new HashSet<String>();
-		int count = 1;
-		
-		linksWord1.add("http://dbpedia.org/resource/"+word1);
-		linksWord2.add("http://dbpedia.org/resource/"+word2);
-		
-		for(String s : linksWord1){
-			for(String s2 : linksWord2){
-				word1ToWord2.addAll(directRelations(s,s2));
-				word2ToWord1.addAll(directRelations(s2,s));
-				System.out.println(count++);
-			}
-		}
-		PrintWriter out = new PrintWriter("temp.txt");
-		out.println(word1+" - "+word2);
-		for(String s : word1ToWord2)
-			out.println(s);
-		out.println("--------------------------");
-		out.println(word2+" - "+word1);
-		for(String s : word2ToWord1)
-			out.println(s);
-		out.close();
+		Boolean flag = checkExists("http://dbpedia.org/resource/"+word1);
+		System.out.println(flag);
  	}
 	
+public static Boolean checkExists(String word) throws RepositoryException{
+		
+		//endpointUrl should be initially asked from the user and saved as a global parameter or user-settings-parameter
+		String endpointUrl = "http://dbpedia.org/sparql"; 
+		Repository repo = new SPARQLRepository(endpointUrl);
+		repo.initialize();
+		RepositoryConnection con = repo.getConnection();
+		
+		String query = "select DISTINCT ?a where "
+			+ "{ <"+word+"> ?a ?r }";
+		
+		try {
+			String queryString = query;
+
+			TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+			TupleQueryResult result = tupleQuery.evaluate();
+			
+			try {
+				if(result.equals(null))
+					return false;
+				else return true;
+//				while (result.hasNext()) { // iterate over the result
+//					BindingSet bindingSet = result.next();
+//					if(bindingSet.size()>0)
+//						return true;
+//					else return false;
+//				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			} finally {
+				result.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			con.close();
+		}
+	}
 	
 public static List<String> directRelations(String url1, String url2) throws RepositoryException{
 
@@ -112,7 +157,7 @@ public static List<String> getResourceLinksForWord(String word) throws Repositor
 	        	+ "FILTER (lang(?label) = 'en'). "
 	        	+ "?label bif:contains \""+word+"\" . "
 	        	+ "?s dcterms:subject ?sub"
-				+ "} LIMIT 5";
+				+ "}";
 		
 		try {
 			String queryString = query;
