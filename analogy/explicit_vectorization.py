eeae4ee293670416dd051577541026305ff28c80
pearsauce <atma.persaud@gmail.com>
2015-04-24 20:45:34 -0700

    fixed some bugs, changed sparse matrix representation to dictionary of keys to enable slicing

--- analogy/explicit_vectorization.py
+++ analogy/explicit_vectorization.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import sys
+import json
 import math
 import argparse
+import datetime
 import numpy as np
 from scipy import sparse
 from scipy.sparse import linalg
@@ -11,33 +13,22 @@ def main():
     infile = open(sys.argv[1])
     outfile = open(sys.argv[2], 'w')
     vocabfile = open(sys.argv[3])
-    vdict = json.load(vocabfile)
+    vocab = json.load(vocabfile)
 
-    F = sparse.coo_matrix((len(vdict), 4*len(vdict)))
+    F = sparse.dok_matrix((len(vdict), 4*len(vdict)), dtype=np.int32)
     corpus_size = 0
+    lc = 0
 
     for line in infile:
+        lc += 1
+        if lc % 10000 == 0:
+            print('processing line ' + str(lc) + ' at ' + str(datetime.datetime.now()))
         words = line.split()
         num_words = len(words)
         corpus_size += num_words
 
-        if num_words < 2:
-            continue
-
-        elif num_words == 2:
-            F[vocab[words[0]], 4 * vocab[words[1]] + 2] += 1
-
-            F[vocab[words[1]], 4 * vocab[words[0]] + 1] += 1
-
-        elif num_words == 3:
-            F[vocab[words[0]], 4 * vocab[words[1]] + 2] += 1
-            F[vocab[words[0]], 4 * vocab[words[2]] + 3] += 1
-
-            F[vocab[words[1]], 4 * vocab[words[0]] + 1] += 1
-            F[vocab[words[1]], 4 * vocab[words[2]] + 2] += 1
-
-            F[vocab[words[2]], 4 * vocab[words[0]] + 0] += 1
-            F[vocab[words[2]], 4 * vocab[words[1]] + 1] += 1
+        if num_words < 5:
+            process_short_line(num_words, words, F, vocab)
 
         else:
             F[vocab[words[0]], 4 * vocab[words[1]] + 2] += 1
@@ -61,10 +52,11 @@ def main():
                 F[vocab[word], 4 * vocab[words[i+2]] + 3] += 1
                 
     # compute PMI
-    word_freqs = F.sum(1)
-    context_freqs = F.sum(0)
+    Fc = F.tocoo()
+    word_freqs = Fc.sum(1)
+    context_freqs = Fc.sum(0)
 
-    for i,j,v in zip(F.row, F.col, F.data):
+    for i,j,v in zip(Fc.row, Fc.col, Fc.data):
         F[i,j] = max( math.log((v * corpus_size) / (word_freqs[i] * context_freqs[j])), 0 )
 
     # compute TruncatedSVD
@@ -77,5 +69,24 @@ def main():
     outfile.close()
     vocabfile.close()
 
+def process_short_line(num_words, words, F, vocab):
+    if num_words < 2:
+        continue
+    
+    elif num_words == 2:
+        F[vocab[words[0]], 4 * vocab[words[1]] + 2] += 1
+        
+        F[vocab[words[1]], 4 * vocab[words[0]] + 1] += 1
+        
+    elif num_words == 3:
+        F[vocab[words[0]], 4 * vocab[words[1]] + 2] += 1
+        F[vocab[words[0]], 4 * vocab[words[2]] + 3] += 1
+        
+        F[vocab[words[1]], 4 * vocab[words[0]] + 1] += 1
+        F[vocab[words[1]], 4 * vocab[words[2]] + 2] += 1
+        
+        F[vocab[words[2]], 4 * vocab[words[0]] + 0] += 1
+        F[vocab[words[2]], 4 * vocab[words[1]] + 1] += 1
+        
 if __name__ == '__main__':
     main()
